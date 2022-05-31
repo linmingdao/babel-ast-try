f9cb18880d48ad07a3a4ae7a4debc98b92c3287d
Linmingdao <mr.lmd@outlook.com>
2022-05-31 19:38:03 +0800

    update

---
+++ src/02_visitorInVisitor.js
@@ -0,0 +1,46 @@
+const parser = require("@babel/parser");
+const types = require("@babel/types");
+const template = require("@babel/template");
+const traverse = require("@babel/traverse");
+const generator = require("@babel/generator");
+
+const code = `
+function square(n) {
+  return n * n;
+}
+`;
+
+const ast = parser.parse(code);
+
+const updateParamNameVisitor = {
+  Identifier(path) {
+    if (path.node.name === this.paramName) {
+      path.node.name = "x";
+    }
+  },
+};
+
+const MyVisitor = {
+  FunctionDeclaration(path) {
+    const param = path.node.params[0];
+    const paramName = param.name;
+    param.name = "x";
+
+    path.traverse(updateParamNameVisitor, { paramName });
+  },
+};
+
+traverse.default(ast, MyVisitor);
+
+const newCode = generator.default(
+  ast,
+  {
+    retainLines: false,
+    compact: "auto",
+    concise: false,
+    quotes: "double",
+  },
+  code
+);
+
+console.log(newCode.code);
